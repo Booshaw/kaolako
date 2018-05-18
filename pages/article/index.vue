675a48c6e8a937471e06862bd6f4cdcb1557c3b8
booshaw <booshaw@booshaw.local>
2018-05-18 11:29:27 +0800

    add article top view

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -1,43 +1,61 @@
 <template>
   <div class="treatment-wrapper">
     <kaola-nav></kaola-nav>
+    <div class="top-box">
+      <div class="c-category-box">
+        <Row>
+          <i-col :lg="8" :md="8" :xs="0" offset="2">
+            <h1 class="title"> <span class="icon"><Icon type="ios-book"></Icon></span> 考拉干货 <span class="slogn">轻松考试<Icon type="link"></Icon>为梦想加油</span>  </h1>
+          </i-col>
+          <i-col :lg="14" :md="14" :xs="24">
+            <div class="search-box">
+              <i-input v-model="search" placeholder="搜索感兴趣的内容">
+                <Button slot="append" icon="ios-search"></Button>
+              </i-input>
+            </div>
+          </i-col>
+        </Row>
+      </div>
+      <div class="category-list">
+        <ul>
+          <li v-for="(item, index) in categoryList" :key="index" :class="{selectCategoryClass: item.id === categoryCurrent}" @click.stop="selectCategory(item)" class="category-li">{{item.name}}</li>
+          <!-- <li @click.stop="moreCategory = !moreCategory" class="arrow" v-if="categoryList.length && moreCategoryIcon && categoryList.length > 6"><Icon type="chevron-down"></Icon></li> -->
+        </ul>
+      </div>
+      <div class="tag-list">
+        <ul>
+          <li v-for="(item, index) in tagList" :key="index" :class="{selectTagClass: item.id === tagCurrent}" @click.stop="selectTag(item)" class="tag-li">{{item.name}}</li>
+          <!-- <li @click.stop="moreTag = !moreTag" class="arrow"><Icon type="chevron-down"></Icon></li>           -->
+        </ul>
+      </div>
+    </div>
     <div class="main">
       <Row>
-      <i-col :lg="18" :md="18" :sm="24" :xs="24">
-        <div class="box">
-          <div class="c-category">
-            <Tabs value="all" size="small" :animated="false">
-              <TabPane label="全部" name="all"> </TabPane>
-              <TabPane :label="item.categoryName" :name="index.toString()" v-for="(item, index) in courseCategoryList" :key="index">
-                <ul class="item-box">
-                  <li v-for="(i, index1) in item.children" :key="index1">{{i.categoryName}}</li>
-                </ul>
-              </TabPane>
-            </Tabs>
+        <i-col :lg="18" :md="18" :sm="24" :xs="24">
+          <div class="box">
+            <article-list :articleList="articleList" @select="selectArticle"></article-list>
+            <div class="pages-wrapper" v-if="pageShow">
+              <Page :total="totalRecord" size="small" transfer show-elevator show-sizer @on-change="pageNum" @on-page-size-change="pageSizeNum"></Page>
+            </div>
+            <!-- <div v-if="!pageShow">
+              <Spin fix>
+                <Icon type="load-c" size=18 class="icon-load"></Icon>
+                <div>Loading</div>
+              </Spin>
+            </div> -->
           </div>
-          <article-list :articleList="articleList" @select="selectArticle"></article-list>
-          <div class="pages-wrapper" v-if="pageShow">
-            <Page :total="totalRecord" size="small" transfer show-elevator show-sizer @on-change="pageNum" @on-page-size-change="pageSizeNum"></Page>
+        </i-col>
+        <i-col :lg="6" :md="6" :sm="24" :xs="24">
+          <div class="adv">
+            <div class="publish">
+              <p class="btn-publish" @click="toPublish">发布手记</p>
+            </div>
+            <div v-if="pop && pop.length">
+              <h2 class="text">精选推荐</h2>
+            <pop-list :popList="pop" @select="selectArticle"></pop-list>
+            </div>
           </div>
-          <!-- <div v-if="!pageShow">
-            <Spin fix>
-              <Icon type="load-c" size=18 class="icon-load"></Icon>
-              <div>Loading</div>
-            </Spin>
-          </div> -->
-        </div>
-      </i-col>
-      <i-col :lg="6" :md="6" :sm="24" :xs="24">
-        <div class="adv">
-          <div class="publish">
-            <p class="btn-publish" @click="toPublish">发布手记</p>
-          </div>
-          <div v-if="pop && pop.length">
-            <h2 class="text">精选推荐</h2>
-          <pop-list :popList="pop" @select="selectArticle"></pop-list>
-          </div>
-        </div>
-      </i-col>
+        </i-col>
       </Row>
     </div>
     <kaola-footer></kaola-footer>
@@ -59,7 +77,16 @@ export default {
       pageShow: false,
       articleList: [],
       pop: [],
-      courseCategoryList: []
+      courseCategoryList: [],
+      search: '', // 搜索内容
+      categoryList: [], // 分类列表
+      categoryCurrent: 0, // 选择的分类
+      moreCategoryIcon: false, // 更多分类icon
+      moreCategory: false, // 更多分类显示
+      tagList: [], // 标签分类
+      moreTagIcon: false, // 更多标签icon      
+      tagCurrent: 0, // 选择的标签
+      moreTag: false // 更多标签显示
     }
   },
   created() {
@@ -72,6 +99,8 @@ export default {
         console.log(res)
         if (res.data.code === 200) {
           this.articleList = res.data.data.articleList
+          this.tagList = res.data.data.tagList
+          this.categoryList = res.data.data.categoryList
           this.pop = res.data.data.articleList
           this.courseCategoryList = res.data.data.courseCategoryList          
           this.pageShow = true
@@ -96,7 +125,13 @@ export default {
       this.$router.push({
         path: '/article/publish'
       })
-    }
+    },
+    selectCategory(item) {
+      this.categoryCurrent = item.id
+    },
+    selectTag(item) {
+      this.tagCurrent = item.id
+    },
   },
   components: {
     ArticleList,
@@ -113,6 +148,96 @@ export default {
   display flex
   flex-direction column
   margin-top 32px
+  .top-box
+    margin-top 28px
+    .c-category-box
+      // height 132px
+      background url('/static/sz-header-bk.png') center center no-repeat
+      background-size cover
+      box-sizing border-box
+      .title
+        margin 20px 0
+        padding 0 20px
+        color #ffffff
+        font-size 32px
+        .icon
+          color #ed3f14
+          font-size 36px
+        .slogn
+          font-size 14px
+      .search-box
+        margin 0 auto
+        padding 35px 0
+        width 60%
+    .category-list
+      margin 20px auto
+      border-bottom 1px solid #eeeeee
+      box-shadow: 0 8px 16px 0 rgba(28, 31, 33, 0.1)
+      ul
+        width 80%
+        margin 0 auto
+        text-align left
+        @media screen and (max-width: 440px)
+          width 100%
+        .category-li
+          display inline-block
+          color #07111b
+          font-size 16px
+          line-height 40px
+          font-weight bold
+          margin-right 48px
+          @media screen and (max-width: 440px)
+            margin 0 8px
+            font-size 14px
+          &:hover
+            cursor pointer
+            color #f01414
+        .arrow
+          display inline-block
+        .selectCategoryClass
+          color #f01414
+          &:hover
+            color #f01414
+      .more-category-box
+        text-align left
+    .tag-list
+      margin 20px 0
+      border-bottom 1px solid #eeeeee
+      text-align left
+      ul
+        width 80%
+        margin 0 auto
+        @media screen and (max-width: 440px)
+          width 100%
+        .tag-li
+          display inline-block
+          color #4d555d
+          font-size 14px
+          padding 0 10px
+          line-height 32px
+          margin 0 8px 8px 0
+          border-radius 16px
+          @media screen and (max-width: 440px)
+            margin 0 8px
+            font-size 14px
+          &:hover
+            cursor pointer
+            background-color #D9DDE1
+        .arrow
+          display inline-block
+          &:hover
+            cursor pointer
+            color #f01414
+        .selectTagClass
+          color #ffffff
+          background-color #2b333b
+          &:hover
+            cursor pointer
+            background-color #2b333b
+      .more-tag-box
+        text-align left
+        li
+          display inline-block
   .main
     flex 1
     width 80%
@@ -122,7 +247,7 @@ export default {
     .box
       margin-right 32px
       text-align left
-      padding 16px
+      // padding 16px
       @media screen and (max-width: 440px)
         margin-right 0
       .c-category
