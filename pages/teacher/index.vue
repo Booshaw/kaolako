9d32d061dc5c827cad04b0be2b21b822ba76c94a
booshaw <booshaw@192.168.0.102>
2018-05-23 22:27:58 +0800

    change teacher's page view

--- pages/teacher/index.vue
+++ pages/teacher/index.vue
@@ -1,25 +1,44 @@
 <template>
   <section class="t-wrapper">
-    <kaola-nav class="nav"></kaola-nav>    
+    <kaola-nav class="nav"></kaola-nav>
     <div class="main">
-      <div class="c-category">
+      <!-- <div class="c-category">
         <Tabs value="all" size="small">
           <TabPane label="全部" name="all"></TabPane>
-          <TabPane :label="item.categoryName" :name="index.toString()" v-for="(item, index) in courseCategoryList" :key="index">
+          <TabPane :label="item.name" :name="index.toString()" v-for="(item, index) in category" :key="index">
             <ul class="item-box">
-              <li v-for="(i, index1) in item.children" :key="index1">{{i.categoryName}}</li>
+              <li v-for="(i, index1) in item.children" :key="index1">{{i.name}}</li>
             </ul>
           </TabPane>
         </Tabs>
+      </div> -->
+      <div class="c-category-box">
+        <Row>
+          <i-col :lg="10" :md="10" :xs="0" offset="2">
+            <h1 class="title"> <span class="icon"><Icon type="ios-flower"></Icon></span> 名师殿堂 <span class="slogn">轻松考试<Icon type="link"></Icon>为梦想加油</span>  </h1>
+          </i-col>
+          <i-col :lg="12" :md="12" :xs="24">
+            <div class="search-box">
+              <i-input v-model="search" placeholder="搜索感兴趣的内容">
+                <Button slot="append" icon="ios-search"></Button>
+              </i-input>
+            </div>
+          </i-col>
+        </Row>
+      </div>
+      <div class="category-list">
+        <ul>
+          <li v-for="(item, index) in category" :key="index" :class="{selectCategoryClass: item.id === categoryCurrent}" @click.stop="selectCategory(item)" class="category-li">{{item.name}}</li>
+          <!-- <li @click.stop="moreCategory = !moreCategory" class="arrow" v-if="categoryList.length && moreCategoryIcon && categoryList.length > 6"><Icon type="chevron-down"></Icon></li> -->
+        </ul>
       </div>
       <div class="teacher">
         <div class="teacher-wrapper">
-          <!-- <h3 class="title">考拉精英名师</h3> -->
           <Row>
             <i-col :lg="4" :md="8" :sm="12" :xs="12" v-for="(t, index4) in teacherList" :key="index4">
               <div class="teacher-intro" @click.stop="toTeacherInfo(t)">
                 <img :src="t.avatar" :alt="t.speciality" class="avatar">
-                <h2 class="name">{{t.name}}</h2>
+                <h2 class="name">{{t.realName}}</h2>
                 <h3 class="spec">{{t.speciality}}</h3>
                 <p class="intro">{{t.introduction}}</p>
               </div>
@@ -42,12 +61,14 @@ import Service from '~/plugins/axios'
 export default {
   data () {
     return {
+      search: '',
       totalRecord: 100,
       pageShow: false,
       page: 1,
       pageSize: 12,
       teacherList: [],
-      courseCategoryList: []
+      category: [], // 师资分类列表
+      categoryCurrent: 0, // 默认分类
     }
   },
   // asyncData() {
@@ -60,17 +81,32 @@ export default {
   //   })
   // },
   created() {
-    this._getCourseList()
+    this._getCategory()
+    this._getTeacherList()
   },
   methods: {
-    _getCourseList() {
-      return Service.get(`https://easy-mock.com/mock/5ac20177470d657aa5c1dd51/kaolako/homePage`)
+    _getCategory() {
+      return Service.post(`http://api.kaolako.com/kaola/common/dict/get`,
+      {
+        dictType: ['courseCategory']
+      })
       .then((res) => {
-        console.log(res)
-        if (res.data.code === 200) {
-          this.teacherList = res.data.data.teacherList
+        console.log(res)        
+        if (res.data.code === '200') {
+          this.category = res.data.data.courseCategory
+        }
+      })
+    },
+    _getTeacherList() {
+      return Service.post(`http://api.kaolako.com/kaola/web/teacher/list`,{
+        id: this.categoryCurrent
+      })
+      .then((res) => {
+        // console.log(res)
+        if (res.data.code === '200') {
+          this.teacherList = res.data.data.pageData
           this.pageShow = true
-          this.courseCategoryList = res.data.data.courseCategoryList
+          // this.courseCategoryList = res.data.data.courseCategoryList
         }
       })
     },
@@ -107,19 +143,107 @@ export default {
   .main
     margin-top 60px
     flex 1 0 auto
-    .c-category
-      width 80%
-      margin 32px auto 16px
+    // .c-category
+    //   width 80%
+    //   margin 32px auto 16px
+    //   text-align left
+    //   @media screen and (max-width: 440px)
+    //     width 100%
+    //     padding 16px
+    //   .item-box
+    //     padding-left 16px
+    //     li
+    //       display inline-block
+    //       margin 8px
+    //       font-size 12px
+    .c-category-box
+      // height 132px
+      background url('/static/sz-header-bk.png') center center no-repeat
+      background-size cover
+      box-sizing border-box
+      .title
+        margin 20px 0
+        padding 0 20px
+        color #ffffff
+        font-size 32px
+        .icon
+          color #ed3f14
+          font-size 36px
+        .slogn
+          font-size 14px
+      .search-box
+        margin 0 auto
+        padding 35px 0
+        width 60%
+    .category-list
+      margin 20px auto
+      border-bottom 1px solid #eeeeee
+      box-shadow 0 8px 16px 0 rgba(28, 31, 33, 0.1)
+      ul
+        width 80%
+        margin 0 auto
+        text-align left
+        @media screen and (max-width: 440px)
+          width 100%
+        .category-li
+          display inline-block
+          color #07111b
+          font-size 16px
+          line-height 40px
+          font-weight bold
+          margin-right 48px
+          @media screen and (max-width: 440px)
+            margin 0 8px
+            font-size 14px
+          &:hover
+            cursor pointer
+            color #f01414
+        .arrow
+          display inline-block
+        .selectCategoryClass
+          color #f01414
+          &:hover
+            color #f01414
+      .more-category-box
+        text-align left
+    .tag-list
+      margin 20px 0
+      border-bottom 1px solid #eeeeee
       text-align left
-      @media screen and (max-width: 440px)
-        width 100%
-        padding 16px
-      .item-box
-        padding-left 16px
+      ul
+        width 80%
+        margin 0 auto
+        @media screen and (max-width: 440px)
+          width 100%
+        .tag-li
+          display inline-block
+          color #4d555d
+          font-size 14px
+          padding 0 12px
+          line-height 32px
+          margin 0 14px 14px 0
+          border-radius 16px
+          @media screen and (max-width: 440px)
+            margin 0 8px
+            font-size 14px
+          &:hover
+            cursor pointer
+            background-color #D9DDE1
+        .arrow
+          display inline-block
+          &:hover
+            cursor pointer
+            color #f01414
+        .selectTagClass
+          color #ffffff
+          background-color #2b333b
+          &:hover
+            cursor pointer
+            background-color #2b333b
+      .more-tag-box
+        text-align left
         li
           display inline-block
-          margin 8px
-          font-size 12px
     .teacher
       width 80%
       margin 0 auto
